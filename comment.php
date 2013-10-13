aff0e1823a80016ab292655be3d8bc7d5f9a0147
Jenton <addie@Adriennes-MacBook-Air.local>
2013-10-12 20:40:59 -0700

    more edits

--- comment.php
+++ comment.php
@@ -15,10 +15,21 @@
 </br>
 <input type="button" value="Send Comment" id="button">-->
 
-reviewID : <input type="text" name="reviewID" id="reviewID">
+<!-- the id="1" will help identify which review ID you're tagging -->
+<div id="1">
+<h1>Review ID 1</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet eleifend velit, et molestie erat auctor in. In sed odio eu ligula consequat rutrum et nec massa. Vivamus id pulvinar magna, blandit blandit sapien. </p>
+<input type="button" value="Food" class="foodButton">
+<input type="button" value="Service" class="serviceButton">
+</div>
+<!--reviewID : 1 <input type="text" name="reviewID" id="reviewID">-->
 
-<input type="button" value="Food" id="foodButton">
-<input type="button" value="Service" id="serviceButton">
+<div id="2">
+<h1>Review ID 2</h1>
+<p id="2">Aenean facilisis tristique ligula eget dignissim. Maecenas nec molestie nibh. Nunc nisi ligula, tristique ut arcu sollicitudin, semper porta nulla. Sed eu ultrices mauris. Praesent condimentum posuere fermentum. Integer rhoncus augue nec orci ultrices, </p> 
+<input type="button" value="Food" class="foodButton">
+<input type="button" value="Service" class="serviceButton">
+</div>
 <div id="info" />
 </form>
 </body>
--- js/script.js
+++ js/script.js
@@ -1,31 +1,46 @@
 $(document).ready(function(){
 
+  //when the page loads, make a call to the database and get the food and service tags
+  $.ajax({
+    type:"post",
+    url:"phpScript.php",
+    data:"action=food"+"&reviewID="+reviewID,
+    success:function(data){
+     $("#info").html(data);
+     //console.log(data);
+   } 
+ });
+
+
+
   // When you click the food button, this code goes
-  $("#foodButton").on("click", function(){
-                          
-                          //pass the number in the reviewID textbox into a variable
-                          var reviewID=$("#reviewID").val();
+  $(".foodButton").on("click", function(){
+  //pass the number in the reviewID textbox into a variable
+  var reviewID = $(this).parent().attr("id");
+  console.log(reviewID);
 
-                          //Ajax call. The data: passes in whatever information you want into the foodButton.php file. You can type in whatever you key you want and you can reference it in the php file
+  //Ajax call. The data: passes in whatever information you want into the foodButton.php file. You can type in whatever you key you want and you can reference it in the php file
 
-                          $.ajax({
-                            type:"post",
-                            url:"foodButton.php",
-                            data:"action=food"+"&reviewID="+reviewID,
-                            success:function(data){
-                             $("#info").html(data);
-                             console.log($re)
-                           }
+  $.ajax({
+    type:"post",
+    url:"phpScript.php",
+    data:"action=food"+"&reviewID="+reviewID,
+    success:function(data){
+     $("#info").html(data);
+       //console.log(data);
+     }
 
-                         });
+   });
 
-                        });
+  });
 
-  $("#serviceButton").on("click", function(){
+  $(".serviceButton").on("click", function(){
 
+                          //unused variables
                           //var name=$("#name").val();
                           //var message=$("#message").val();
-                          var reviewID=$("#reviewID").val();
+
+                          var reviewID = $(this).parent().attr("id");
 
                           $.ajax({
                             type:"post",
